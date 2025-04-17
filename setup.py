099e8bc3ad0ad0c6894dfbdb3ae8e8c9c328b9c7
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-17 10:00:47 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="auth-brilliant-database",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Django and NumPy based auth database in Python",
+    keywords="auth-brilliant-database, python",
+    url="",
+)
